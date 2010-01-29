c65a1e409ffc3ef81e4840715eb493e6b7139886
japanrock <japanrock@gmail.com>
2010-01-29 23:12:54 +0900

    first commit

--- lr_bot.rb
+++ lr_bot.rb
@@ -6,79 +6,56 @@ require 'oauth'
 require 'json'
 require 'hpricot'
 require 'open-uri'
-require "rexml/document"
+require 'yaml'
 
-# トークンセット
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+class LiveRevolution
+  def initialize
+    @secret_keys = YAML.load_file("secret_keys.yml.example")
+  end
+  
+  def consumer_key
+    @secret_keys["ConsumerKey"]
+  end
 
-# オブジェクト生成
-consumer = OAuth::Consumer.new(
-  CONSUMER_KEY,
-  CONSUMER_SECRET,
-  :site => 'http://twitter.com'
-)
-access_token = OAuth::AccessToken.new(
-  consumer,
-  ACCESS_TOKEN,
-  ACCESS_TOKEN_SECRET
-)
+  def consumer_secret
+    @secret_keys["ConsumerSecret"]
+  end
 
-# Feed
-base_url      = "http://www.live-revolution.co.jp"
-lr_news_feed  = Hpricot(open("#{base_url}/atom_0093news.xml"))
-adc_news_feed = Hpricot(open("#{base_url}/atom_0060adc_news.xml"))
+  def access_token
+    @secret_keys["AccessToken"]
+  end
 
-#Parse
-#lr_news_feed_parse = REXML::Document.new(lr_news_feed)
-#lr_news_feed_parse = REXML::Document.new(source)
+  def access_token_secret
+    @secret_keys["AccessTokenSecret"]
+  end
 
-p lr_news_feed
+  def consumer
+    consumer = OAuth::Consumer.new(
+      CONSUMER_KEY,
+      CONSUMER_SECRET,
+      :site => 'http://twitter.com'
+    )
+  end
 
-# diary_list      = doc.search("//div[@id='article_title_in_list']")                  # タイトルとリンクを取得のためのパース
-# diary_time_list = doc.search("//div[@id='article_information_in_list']")            # 時間を取得のためのパース
-# diaries         = {}                                                                # 日記のタイトル、リンク、時間を入れる箱
-# 
-# 
-# # タイトルとリンクを取得
-# count = 0
-# diary_list.each do |diary|
-#   doc = Hpricot(diary.inner_html)
-#   
-#   # http://ruby.g.hatena.ne.jp/garyo/20061207/1165477582
-#   hrefs  = (doc/:a).map {|elem| elem[:href]}
-# 
-#   # title,link
-#   hrefs.each do |link|
-#     diaries[count] = ["#{(doc/'a').inner_html}","#{base_url}#{link}"]
-#   end
-# 
-#   count = count + 1
-# end
-# 
-# 
-# # 日記の時間を取得
-# count = 0
-# diary_time_list.each do |time|
-#   doc = Hpricot(time.inner_html)
-#   # 09月08日02:57 => 20099080257
-#   diaries[count] << "#{(doc).inner_html.gsub(/ |月|日|:/, '')}"     # title,link,更新時間
-#   count = count + 1
-# end
-# 
-# 
-# # 更新日時が今から１時間以内なら、Twitterにポスト
-# now = Time.new
-# an_hour_ago = now - 3600
-# 
-# count.times do |i|
-#   if diaries[i][2].gsub(/\n/,'').to_i > an_hour_ago.strftime("%m%d%H%M").to_i
-#      response = access_token.post(
-#       'http://twitter.com/statuses/update.json',
-#       'status'=> "#{diaries[i][0]} #{diaries[i][1]}"
-#     )
-#   end
-# end
-# 
+  def access_token
+    access_token = OAuth::AccessToken.new(
+      consumer,
+      ACCESS_TOKEN,
+      ACCESS_TOKEN_SECRET
+    )
+  end
+
+  def base_url
+    "http://www.live-revolution.co.jp"
+  end
+
+  def news_feed
+    Hpricot(open("#{base_url}/atom_0093news.xml"))
+  end
+
+  def adc_news_feed
+    Hpricot(open("#{base_url}/atom_0060adc_news.xml"))
+  end
+end
+
+live_revolution = LiveRevolution.new
