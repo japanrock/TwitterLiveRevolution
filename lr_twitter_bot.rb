bd3068cb9b2ed2bfe5389e8329a7033b16138e44
福重 伸太朗 <japanrock@fukushige-shintarou-no-iMac.local>
2010-04-27 09:00:12 +0900

    外部ファイルをrequreして、TwitterOauthクラスを使うようにした

--- lr_twitter_bot.rb
+++ lr_twitter_bot.rb
@@ -9,60 +9,16 @@ require 'open-uri'
 require 'yaml'
 require 'parsedate'
 require "kconv"
-
-### TODO:
-### ・TwitterBaseクラスを外に出す
+require File.dirname(__FILE__) + '/twitter_oauth'
 
 # Usage:
-# ruby lr_twitter.rb /path/to/sercret_keys.yml
-
-# TwitterのAPIとのやりとりを行うクラス
-class TwitterBase
-  def initialize
-    # config.yml内のsercret_keys.ymlをloadします。
-    @secret_keys = YAML.load_file(ARGV[0] || 'sercret_keys.yml')
-  end
-  
-  def consumer_key
-    @secret_keys["ConsumerKey"]
-  end
-
-  def consumer_secret
-    @secret_keys["ConsumerSecret"]
-  end
-
-  def access_token_key
-    @secret_keys["AccessToken"]
-  end
-
-  def access_token_secret
-    @secret_keys["AccessTokenSecret"]
-  end
-
-  def consumer
-    @consumer = OAuth::Consumer.new(
-      consumer_key,
-      consumer_secret,
-      :site => 'http://twitter.com'
-    )
-  end
-
-  def access_token
-    consumer
-    access_token = OAuth::AccessToken.new(
-      @consumer,
-      access_token_key,
-      access_token_secret
-    )
-  end
-
-  def post(tweet=nil)
-    response = access_token.post(
-      'http://twitter.com/statuses/update.json',
-      'status'=> tweet
-    )
-  end
-end
+#  1. このファイルと同じディレクトリに以下３つのファイルを設置します。
+#   * twitter_oauth.rb
+#   * http://github.com/japanrock/TwitterTools/blob/master/twitter_oauth.rb
+#   * sercret_key.yml
+#   * http://github.com/japanrock/TwitterTools/blob/master/secret_keys.yml.example
+#  2. このファイルを実行します。
+#   ruby lr_twitter_bot.rb
 
 # フィードを扱う基本クラス
 class Feed
@@ -259,39 +215,39 @@ class PresidentVision < Feed
 end
 
 
-twitter_base     = TwitterBase.new
+twitter_oauth = TwitterOauth.new
 
 # Live Revolution ADC++ Maintenance News Feed Post
 live_revolution  = LiveRevolution.new
 live_revolution.adc_maintenance_news_feed
 live_revolution.titles.each_with_index do |title, index|
-  twitter_base.post(title + " - " + live_revolution.links[index])
+  twitter_oauth.post(title + " - " + live_revolution.links[index])
 end
 
 # Live Revolution ADC++ News Feed Post
 live_revolution  = LiveRevolution.new
 live_revolution.adc_news_feed
 live_revolution.titles.each_with_index do |title, index|
-  twitter_base.post(title + " - " + live_revolution.links[index])
+  twitter_oauth.post(title + " - " + live_revolution.links[index])
 end
 
 # Live Revolution News Feed Post
 live_revolution  = LiveRevolution.new
 live_revolution.news_feed
 live_revolution.titles.each_with_index do |title, index|
-  twitter_base.post(title + " - " + live_revolution.links[index])
+  twitter_oauth.post(title + " - " + live_revolution.links[index])
 end
 
 # President Vision Feed Post
 president_vision = PresidentVision.new
 president_vision.feed
 president_vision.titles.each_with_index do |title, index|
-  twitter_base.post(president_vision.header + president_vision.descriptions[index] + " - " + president_vision.links[index])
+  twitter_oauth.post(president_vision.header + president_vision.descriptions[index] + " - " + president_vision.links[index])
 end
 
 # President Blog Feed Post
 president_blog   = PresidentBlog.new
 president_blog.feed
 president_blog.titles.each_with_index do |title, index|
-  twitter_base.post(president_blog.header + title + " - " + president_blog.links[index])
+  twitter_oauth.post(president_blog.header + title + " - " + president_blog.links[index])
 end
